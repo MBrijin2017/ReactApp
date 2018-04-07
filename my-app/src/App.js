b2dda413adc08d6e7a2c9ea23e1a4d93cce19276
MbF-Brijin <brijin@madebyfire.com>
2018-04-07 15:48:13 +0530

    routingadded

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+import home from './Components/Home';
+import Aboutus from './Components/AboutUs';
+import Resource from './Components/Gallary';
+import contuctus from './Components/Contact';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+    	<div>
+		    <Router>
+				<div className="container">
+					<header>
+						<nav>
+				  		<ul>
+				  			<li><Link to="/home">Home</Link></li>
+							<li><Link to="/Aboutus">About Us</Link></li>
+							<li><Link to="/Resource">Gallary</Link></li>
+							<li><Link to="/contuctus">Contact Us</Link></li>
+				  		</ul>
+					  	</nav>
+					</header>
+					<Route path="/home" component={home} />
+					<Route path="/aboutus" component={Aboutus} />
+					<Route path="/resource" component={Resource} />
+					<Route path="/contuctus" component={contuctus}/>
+				</div>
+		    </Router>
+		    
+		</div>
     );
   }
 }
